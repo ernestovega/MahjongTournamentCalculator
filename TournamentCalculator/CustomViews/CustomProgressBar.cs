184caf100f83e84f274676bdf508fb17e485849a
Eto <ernestovega85@gmail.com>
2016-10-02 19:17:40 +0200

    customProgressBar

--- TournamentCalculator/CustomViews/CustomProgressBar.cs
+++ TournamentCalculator/CustomViews/CustomProgressBar.cs
@@ -1,24 +1,59 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace MahjongTournamentCalculator.CustomViews
 {
+    public enum ProgressBarDisplayText
+    {
+        Percentage,
+        CustomText
+    }
+
     class CustomProgressBar : ProgressBar
     {
+        //Property to set to decide whether to print a % or Text
+        public ProgressBarDisplayText DisplayStyle { get; set; }
+
+        //Property to hold the custom text
+        public String CustomText { get; set; }
+
         public CustomProgressBar()
         {
-            SetStyle(ControlStyles.UserPaint, true);
-            BackColor = Color.White;
+            // Modify the ControlStyles flags
+            //http://msdn.microsoft.com/en-us/library/system.windows.forms.controlstyles.aspx
+            SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true);
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            Rectangle rec = new Rectangle(0, 0, this.Width, this.Height);
-            if (ProgressBarRenderer.IsSupported)
-                ProgressBarRenderer.DrawHorizontalBar(e.Graphics, rec);
-            rec.Width = (int)(rec.Width * ((double)Value / Maximum)) - 4;
-            rec.Height = rec.Height - 4;
-            e.Graphics.FillRectangle(Brushes.Red, 2, 2, rec.Width, rec.Height);
+            System.Drawing.Rectangle rect = ClientRectangle;
+            Graphics g = e.Graphics;
+
+            ProgressBarRenderer.DrawHorizontalBar(g, rect);
+            rect.Inflate(-3, -3);
+            if (Value > 0)
+            {
+                // As we doing this ourselves we need to draw the chunks on the progress bar
+                System.Drawing.Rectangle clip = new System.Drawing.Rectangle(rect.X, rect.Y, (int)Math.Round(((float)Value / Maximum) * rect.Width), rect.Height);
+                ProgressBarRenderer.DrawHorizontalChunks(g, clip);
+            }
+
+            // Set the Display text (Either a % amount or our custom text
+            string text = Value + " / " + Maximum;//DisplayStyle == ProgressBarDisplayText.Percentage ? Value.ToString() + '%' : CustomText;
+
+
+            using (System.Drawing.Font f = new System.Drawing.Font(FontFamily.GenericSerif, 9, FontStyle.Bold))
+            {
+
+                SizeF len = g.MeasureString(text, f);
+                // Calculate the location of the text (the middle of progress bar)
+                // Point location = new Point(Convert.ToInt32((rect.Width / 2) - (len.Width / 2)), Convert.ToInt32((rect.Height / 2) - (len.Height / 2)));
+                System.Drawing.Point location = new System.Drawing.Point(Convert.ToInt32((Width / 2) - len.Width / 2), Convert.ToInt32((Height / 2) - len.Height / 2));
+                // The commented-out code will centre the text into the highlighted area only. This will centre the text regardless of the highlighted area.
+                // Draw the custom text
+                g.DrawString(text, f, Brushes.Black, location);
+            }
         }
     }
 }
